6f265ebd8e5aa7df6307805ee6023ea1896ef750
Fabian Tamp <fabian.tamp@gmail.com>
2016-03-20 15:51:36 +1100

    Stereo sawtooth

--- main.go
+++ main.go
@@ -8,14 +8,20 @@ import (
 
 const sampleRate = 44100
 
-var signal = float32(-1)
+var left = float32(-1)
+var right = float32(-1)
 
-func sawTooth(out []float32) {
-	for i := 0; i < len(out); i++ {
-		out[i] = signal
-		signal += 0.05
-		if signal > 1 {
-			signal -= 2
+func sawTooth(out [][]float32) {
+	for i := 0; i < len(out[0]); i++ {
+		out[0][i] = left
+		out[1][i] = right
+		left += 0.025
+		right += 0.03
+		if left > 1 {
+			left -= 2
+		}
+		if right > 1 {
+			right -= 2
 		}
 	}
 }
@@ -25,7 +31,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	stream, err := portaudio.OpenDefaultStream(0, 1, sampleRate, portaudio.FramesPerBufferUnspecified, sawTooth)
+	stream, err := portaudio.OpenDefaultStream(0, 2, sampleRate, portaudio.FramesPerBufferUnspecified, sawTooth)
 	if err != nil {
 		panic(err)
 	}
@@ -33,5 +39,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	time.Sleep(5 * time.Second)
+	time.Sleep(3 * time.Second)
 }
